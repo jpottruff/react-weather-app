a1e0cf49916bf6b0a9e6c908f586c09a0c0c7004
Jeff Pottruff <jeffpottruff@gmail.com>
2022-07-17 17:08:36 -0400

    add more boilerplate to search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,26 @@
-const Search = () => {
-    return ('Hello');
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        // RAPID API GEODB CITIES CODE GOES HERE
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+    return (
+        <AsyncPaginate
+            placeholder="Search for City"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    );
 }
 
 export default Search;
